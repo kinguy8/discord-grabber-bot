b75671ce82e9c50dfc338119f82b73402cc3b64d
kinguy8 <bogdan.sautski199@gmail.com>
2024-04-03 14:04:12 +0300

    vercel config updates

--- server/src/mongoose/index.ts
+++ server/src/mongoose/index.ts
@@ -2,9 +2,11 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
+    mongoose.set("strictQuery", false);
     console.log('Try to connect to database');
-    await mongoose.connect((process.env.MONGODB_URI as string));
-    console.log('connected...');
+    await mongoose.connect((process.env.MONGODB_URI as string), () => {
+      console.log("Connected to MongoDB");
+    });
   } catch (e) {
     console.log('e', e);
   }
