ed70ca2e59f4a6bc387754bcf347e5559efc237c
kinguy8 <bogdan.sautski199@gmail.com>
2024-04-03 13:06:05 +0300

    remove mongoose

--- server/src/mongoose/index.ts
+++ server/src/mongoose/index.ts
@@ -1,11 +1,10 @@
-import mongoose from 'mongoose';
+import { MongoClient } from 'mongodb';
 
 export const connectDB = async (callback: () => void) => {
   try {
     console.log('Try to connect to database');
-    await mongoose.connect((process.env.MONGODB_URI as string) ?? '',
-      {},
-    );
+    const client = new MongoClient((process.env.MONGODB_URI as string) ?? '');
+    await client.connect();
     console.log('connected...');
     callback();
   } catch (e) {
