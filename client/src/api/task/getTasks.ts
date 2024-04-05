4b9bd2cde79169a359092c4e8338fb6235d24680
kinguy8 <bogdan.sautski199@gmail.com>
2024-04-05 16:52:20 +0300

    add console log

--- client/src/api/task/getTasks.ts
+++ client/src/api/task/getTasks.ts
@@ -1,5 +1,6 @@
 export const getTasks = async () => {
   const response = await fetch(`${process.env.BASE_URL}/api/v1/tasks`);
   const data = await response.json();
+  console.log('data == ', data);
   return data;
 };
